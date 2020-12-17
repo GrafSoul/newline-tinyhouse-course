907cc97f16430d1b39e4d188ca4c9d24e3618641
GrafSoul <grafsoul@gmail.com>
2020-12-18 00:05:42 +0500

    feat: Upadate README.md file.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Building GraphQL Server with Node and TypeScript
 
 Сode for a small training course - <a href="https://www.newline.co/" target="_blank">"Building Your First GraphQL Server with Node and TypeScript"</a>
+
+© 2020 | Building GraphQL Server with Node and TypeScript
--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,11 @@
-const express = require('express');
+// Core
+const express = require("express");
 const app = express();
 const port = 9000;
 
-app.get('/', (req, res) => res.send('hello world'));
+// Routes
+app.get("/", (req, res) => res.send("hello world"));
 app.listen(port);
 
+// Message
 console.log(`[app] : http://localhost:${port}`);
