03bd0dda9afc4a6740a4b0ec20241adc0b5217ec
GrafSoul <grafsoul@gmail.com>
2020-12-18 16:50:59 +0500

    feat: Added Eslint to the project.

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,8 +5,8 @@ const app = express();
 const port = 9000;
 
 // Routes
-const one: number = 1;
-const two: number = 2;
+const one = 1;
+const two = 2;
 
 app.get("/", (_req, res) => res.send(`1 + 2 = ${one + two}`));
 app.listen(port);
