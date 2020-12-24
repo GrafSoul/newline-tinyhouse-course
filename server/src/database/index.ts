6cc9cd34edbec57c38b1795695695ae5200e6af3
GrafSoul <grafsoul@gmail.com>
2020-12-24 17:21:10 +0500

    fix: Fixed minor bugs.

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -9,7 +9,6 @@ const cluster = process.env.CLUSTER;
 const url = 
 `mongodb+srv://${user}:${userPassword}${cluster}.nvckw.mongodb.net/<dbname>?retryWrites=true&w=majority`;
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const connectDatabase = async () => {
     const client = await MongoClient.connect(url, { 
         useNewUrlParser: true,
